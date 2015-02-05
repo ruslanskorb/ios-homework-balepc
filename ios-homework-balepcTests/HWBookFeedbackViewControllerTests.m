773ad9eff10d091337779f36c52fd12e747aaea9
Ruslan Skorb <ruslan.skorb@gmail.com>
2015-02-05 17:14:19 +0300

    [Book Feedback] Adjust the layout of static subviews before tests.

--- ios-homework-balepcTests/HWBookFeedbackViewControllerTests.m
+++ ios-homework-balepcTests/HWBookFeedbackViewControllerTests.m
@@ -30,6 +30,7 @@ describe(@"feedback for the book", ^{
         UIStoryboard *mainStoryboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
         controller = [mainStoryboard instantiateViewControllerWithIdentifier:@"HWBookFeedbackViewControllerID"];
         [controller view]; // loads view and calls viewDidLoad
+        [controller.view layoutIfNeeded]; // lays out the static subviews
         
         name = @"User Name";
         comment = @"Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.";
